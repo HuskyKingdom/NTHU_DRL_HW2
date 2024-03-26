defdfe6bf9b2d77cde7e7907ba776e9eed95e8f3
Yuhang Song <1595025647@qq.com>
2024-03-26 17:19:02 +0800

    update

--- 112062892_hw2_test.py
+++ 112062892_hw2_test.py
@@ -4,7 +4,7 @@ import gym
 class Agent(object):
     """Agent that acts randomly."""
     def __init__(self):
-        self.action_space = gym.spaces.Discrete(2)
+        self.action_space = gym.spaces.Discrete(12)
         pass
 
     def act(self, observation):
--- eval.py
+++ eval.py
@@ -7,9 +7,9 @@ import argparse
 
 from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
-from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+from gym_super_mario_bros.actions import COMPLEX_MOVEMENT
 env = gym_super_mario_bros.make('SuperMarioBros-v0')
-env = JoypadSpace(env, SIMPLE_MOVEMENT)
+env = JoypadSpace(env, COMPLEX_MOVEMENT)
 
 
 
