628fb0487e30704b0146f8968faa65f5ea866e8d
Yuhang Song <1595025647@qq.com>
2024-03-26 11:30:33 +0800

    submission

--- 112062892_hw2_test.py
+++ 112062892_hw2_test.py
@@ -5,6 +5,7 @@ class Agent(object):
     """Agent that acts randomly."""
     def __init__(self):
         self.action_space = gym.spaces.Discrete(2)
+        pass
 
     def act(self, observation):
         return self.action_space.sample()
